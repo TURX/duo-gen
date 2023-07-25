f7eebb66f40d76ebf3b393cc8fd448fb0d439217
Ruixuan Tu <turx2003@gmail.com>
2023-07-25 22:10:31 +0800

    sync upstream

--- src/duo.ts
+++ src/duo.ts
@@ -7,16 +7,48 @@ export default class Duo {
      * When the user submits an activation link, try to fetch the HOTP secret from Duo.
      */
     public static async fetchHOTPSecret(link: string) {
-        let host = "api" + link.substring(link.indexOf("-"), link.indexOf("com") + 3);
-        let key = link.substring(link.lastIndexOf("/") + 1);
-        let duoURL = "https://" + host + "/push/v2/activation/" + key + "?customer_protocol=1";
+        const host = "api" + link.substring(link.indexOf("-"), link.indexOf("com") + 3);
+        const key = link.substring(link.lastIndexOf("/") + 1);
+        const duoURL = "https://" + host + "/push/v2/activation/" + key;
+
+        // https://stackoverflow.com/a/55188241
+        const keys = await crypto.subtle.generateKey(
+            {
+                name: 'RSASSA-PKCS1-v1_5',
+                modulusLength: 2048,
+                publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+                hash: { name: 'SHA-256' },
+            },
+            false,
+            ['sign', 'verify'],
+        );
+        const pubKey = await crypto.subtle.exportKey('spki', keys.publicKey);
+        const pem = `-----BEGIN PUBLIC KEY-----\n${btoa(String.fromCharCode(...new Uint8Array(pubKey))).match(/.{1,64}/g).join('\n')}\n-----END PUBLIC KEY-----`;
 
         const duoData = await fetch(duoURL, {
-            method: "POST",
+            method: 'POST',
             headers: {
-                "Content-type": "application/x-www-form-urlencoded"
+                'Content-Type': 'application/x-www-form-urlencoded'
             },
-            body: "jailbroken=false&architecture=arm64&region=US&app_id=com.duosecurity.duomobile&full_disk_encryption=true&passcode_status=true&platform=Android&app_version=3.49.0&app_build_number=323001&version=11&manufacturer=unknown&language=en&model=Easy%20Duo%20Authentication&security_patch_level=2021-02-01"
+            body: new URLSearchParams({
+                'customer_protocol': '1',
+                'pubkey': pem,
+                'pkpush': 'rsa-sha256',
+                'jailbroken': 'false',
+                'architecture': 'arm64',
+                'region': 'US',
+                'app_id': 'com.duosecurity.duomobile',
+                'full_disk_encryption': 'true',
+                'passcode_status': 'true',
+                'platform': 'Android',
+                'app_version': '3.49.0',
+                'app_build_number': '323001',
+                'version': '11',
+                'manufacturer': 'unknown',
+                'language': 'en',
+                'model': 'Easy Duo Authentication',
+                'security_patch_level': '2021-02-01'
+            })
         });
 
         const duoPayload = await duoData.json();
